cb03684556091f76e01e8eaec0e5c69bdea81df5
lelelol <leandrofortunatojf@gmail.com>
2025-09-23 21:35:17 -0300

    cria projeto reactjs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Disciplina from './Disciplina'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <Disciplina />
+    </>
+  )
+}
+
+export default App
---
+++ src/Disciplina.tsx
@@ -0,0 +1,14 @@
+import "./Disciplina.css"
+function Disciplina() {
+    return (
+        <div>
+            <div>
+                DCC027
+            </div>
+            <div>
+                Desenvolvimento Web Front-End
+            </div>
+        </div>
+    );
+}
+export default Disciplina;
